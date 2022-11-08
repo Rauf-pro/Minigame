ca91b7f9ae8e56d13115322d8b85a23ad97263a1
rrauf8178 <rrauf8178@gmail.com>
2022-11-09 00:06:45 +0400

    added global style, title, gamescreen functionalities

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,9 +1,8 @@
 import { View, Text, Pressable, StyleSheet } from "react-native"; // Creating Custom button
+import Colors from "../../constants/colors";
 
-function PrimaryButton({ children }) {
-  function pressHandler() {
-    console.log("Pressed!");
-  }
+function PrimaryButton({ children, onPress }) {
+  
 
   return (
     <View style={styles.buttonOuterContainer}>
@@ -13,8 +12,8 @@ function PrimaryButton({ children }) {
             ? [styles.buttonInnerContainer, styles.pressed]    // Style for android and ios 
             : styles.buttonInnerContainer
         }
-        onPress={pressHandler}
-        android_ripple={{ color: "#640233" }}
+        onPress={onPress}
+        android_ripple={{ color: Colors.primary600 }}
       >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
@@ -31,7 +30,7 @@ const styles = StyleSheet.create({
     overflow: "hidden",
   },
   buttonInnerContainer: {
-    backgroundColor: "#72063c",
+    backgroundColor: Colors.primary500,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
